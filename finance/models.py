b03af7330a4cacac5c0deea0e7d04fbcc1063fee
alizawx <mralirezanzriii@gmail.com>
2026-07-09 21:24:51 +0330

    add Transaction model

--- finance/models.py
+++ finance/models.py
@@ -54,3 +54,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Transaction(models.Model):
+
+    account = models.ForeignKey(Account,on_delete=models.CASCADE,related_name="transactions")
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="transactions")
+    amount = models.DecimalField(max_digits=15,decimal_places=2)
+    description = models.CharField(max_length=100, blank=True)
+    transaction_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-transaction_date']
+
+    def __str__(self):
+        return f"{self.category.name} {self.amount}"
+
+
+
+
