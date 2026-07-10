caf523d42f76021bf3d2faf26199075bb755c0e3
alizawx <mralirezanzriii@gmail.com>
2026-07-10 13:02:36 +0330

    add admin panel

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Account, Category, Transaction
 
 # Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "account_name",
+        "account_type",
+        "currency",
+        "opening_balance",
+        "account_status",
+    )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category_type",
+        "user",
+        "is_active",
+    )
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "account",
+        "category",
+        "amount",
+        "transaction_date",
+    )
